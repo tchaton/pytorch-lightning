0c9ce11cf503b8e2106d92826e2ac67cdff56b52
tchaton <thomas@grid.ai>
2020-12-07 17:03:37 +0000

    i

---
+++ tests/backends/test_multi_nodes_gpu.py
@@ -0,0 +1,39 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import os
+ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+sys.path.insert(0, ROOT)
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+import torch
+from argparse import ArgumentParser
+import pytorch_lightning as pl
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from pytorch_lightning import LightningModule
+from torch.utils.data import Dataset
+from tests.base.boring_model import *
+
+
+@pytest.mark.skipif(not os.getenv("PL_RUNNING_SPECIAL_TESTS", '0') == '1', reason="test should be run outside of pytest")
+def multi_node_testing(tmpdir):
+    pl.seed_everything(1234)
+    parser = ArgumentParser()
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser = pl.Trainer.add_argparse_args(parser)
+    args = parser.parse_args("--num_nodes=2 --gpus=1 --accelerator=ddp --max_epochs 2".split(" "))
+    model = BoringModel()
+    trainer = pl.Trainer.from_argparse_args(args)
+    trainer.fit(model)
+
