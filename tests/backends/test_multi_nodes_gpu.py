ecc54b45141720e764f2afcd83eb90d1f4cf581f
tchaton <thomas@grid.ai>
2020-12-07 18:49:53 +0000

    i

--- tests/backends/test_multi_nodes_gpu.py
+++ tests/backends/test_multi_nodes_gpu.py
@@ -16,9 +16,13 @@ import os
 ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 sys.path.insert(0, ROOT)
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+import pytest
 import torch
+from unittest import mock
 from argparse import ArgumentParser
 import pytorch_lightning as pl
+from pytorch_lightning import Trainer
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
 from pytorch_lightning import LightningModule
@@ -27,13 +31,36 @@ from tests.base.boring_model import *
 
 
 @pytest.mark.skipif(not os.getenv("PL_RUNNING_SPECIAL_TESTS", '0') == '1', reason="test should be run outside of pytest")
-def multi_node_testing(tmpdir):
-    pl.seed_everything(1234)
-    parser = ArgumentParser()
-    parser.add_argument('--batch_size', default=32, type=int)
-    parser = pl.Trainer.add_argparse_args(parser)
-    args = parser.parse_args("--num_nodes 2 --gpus 1 --accelerator ddp --max_epochs 2".split(" "))
-    model = BoringModel()
-    trainer = pl.Trainer.from_argparse_args(args)
+def test_logging_sync_dist_true_cpu(tmpdir):
+    """
+    Tests to ensure that the sync_dist flag works with CPU (should just return the original value)
+    """
+    fake_result = 1
+
+    class TestModel(BoringModel):
+        def training_step(self, batch, batch_idx):
+            acc = self.step(batch[0])
+            self.log('foo', torch.tensor(fake_result), on_step=False, on_epoch=True, sync_dist=True, sync_dist_op='sum')
+            return acc
+
+        def validation_step(self, batch, batch_idx):
+            output = self.layer(batch)
+            loss = self.loss(batch, output)
+            self.log('bar', torch.tensor(fake_result), on_step=False, on_epoch=True, sync_dist=True, sync_dist_op='sum')
+            return {"x": loss}
+
+    model = TestModel()
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        limit_train_batches=1,
+        limit_val_batches=1,
+        max_epochs=2,
+        weights_summary=None,
+        accelerator="ddp",
+        gpus=1,
+        num_nodes=2,
+    )
     trainer.fit(model)
 
+    assert trainer.logged_metrics['foo'] == fake_result
+    assert trainer.logged_metrics['bar'] == fake_result
